2159f3666001c8a408c301b5fc9c2ea0ae96122b
Matias Quesada <matiasquesadav@gmail.com>
2022-10-03 23:20:10 -0600

    hola 1

--- Proyecto1 Parqueo/Parqueo.cpp
+++ Proyecto1 Parqueo/Parqueo.cpp
@@ -1,1 +1,24 @@
 #include "Parqueo.h"
+
+Parqueo::Parqueo(int n) {
+	vecP = new InfoDelCampo * [n];
+	cant = 0;
+	tam = 0;
+	for (int i = 0; i < n; i++)
+		vecP[i] = NULL;
+}
+Parqueo:: ~Parqueo() {
+	if (vecP != NULL)
+		delete vecP;
+}
+
+int Parqueo::getCant() { return cant; }
+int Parqueo::getTam() { return tam; }
+
+string Parqueo::toString() {
+	stringstream s;
+	s << "-------------PARQUEO----------------" << endl;
+	for (int i = 0; i < cant; i++)
+		s << vecP[i]->toString() << endl;
+	return s.str();
+}
--- Proyecto1 Parqueo/Parqueo.h
+++ Proyecto1 Parqueo/Parqueo.h
@@ -1,9 +1,19 @@
 #pragma once
+#include "InfoDelCampo.h"
 class Parqueo
 {
 private:
-
+	InfoDelCampo** vecP;
+	int cant;
+	int tam;
 public:
+	Parqueo(int n);
+	virtual ~Parqueo();
+
+	int getCant();
+	int getTam();
+	
+	string toString();
 
 };
 
